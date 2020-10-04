d6c540d9d5e07eb3712e0a2e4c011f55cb973665
volodymyrrozdolsky <rozdolskyvolodymyr@gmail.com>
2020-10-03 22:14:21 -0400

    project setup - junit4 configuration - client config - creating user test

---
+++ src/main/java/com/hoaxify/User/User.java
@@ -0,0 +1,13 @@
+package com.hoaxify.User;
+
+import lombok.*;
+
+@Data
+public class User {
+
+    private String username;
+    private String displayName;
+    private String password;
+
+
+}
---
+++ src/main/java/com/hoaxify/User/UserController.java
@@ -0,0 +1,13 @@
+package com.hoaxify.User;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @PostMapping("/api/v1/users")
+    public void createUser(){
+
+    }
+}
