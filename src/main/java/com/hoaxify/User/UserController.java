e51272a4e65135aeac00450c609a62f0bb57cd45
volodymyrrozdolsky <rozdolskyvolodymyr@gmail.com>
2020-10-09 15:51:56 -0400

    PagableDefalut in the controller level

--- src/main/java/com/hoaxify/User/UserController.java
+++ src/main/java/com/hoaxify/User/UserController.java
@@ -8,6 +8,7 @@ import com.hoaxify.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -37,7 +38,7 @@ public class UserController {
             return new GenericResponse("User Saved");
     }
     @GetMapping("/users")
-    public Page<UserVM> getUsers(Pageable page){
+    public Page<UserVM> getUsers(@PageableDefault(size = 10) Pageable page){
        return userService.getUsers(page).map(UserVM::new);
     }
 
