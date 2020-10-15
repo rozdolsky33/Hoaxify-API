e3c1e424ec250aa21a32f82247cfb500c394a3ee
volodymyrrozdolsky <rozdolskyvolodymyr@gmail.com>
2020-10-14 22:00:56 -0400

    uploadFolderMustExist

--- src/test/java/com/hoaxify/StaticResourcesTest.java
+++ src/test/java/com/hoaxify/StaticResourcesTest.java
@@ -4,7 +4,6 @@ package com.hoaxify;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.FilteredClassLoader;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
